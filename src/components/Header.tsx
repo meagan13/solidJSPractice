436d802aea658b49b86ee835419b30a1d65fbada
Meagan <meagan.h.smith@gmail.com>
2023-02-22 10:31:25 -0600

    Find where to change default Bootstrap colors

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
-        <div>
-            <h1>Header</h1>
+        <div >
+            <h1><a href="https://www.solidjs.com/guides/getting-started" target="_blank">Header</a></h1>
         </div>
     )
 }
